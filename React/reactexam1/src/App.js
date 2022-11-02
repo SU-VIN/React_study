59c546c02f5e96153d40cf4efe40c793c4285b1f
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-02 17:33:15 +0900

    JSX

--- React/reactexam1/src/App.js
+++ React/reactexam1/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
 function App() {
   let name = "suvin"
+
   return (
     <div className="App">
-      <header className="App-header">
+      <MyHeader />
         <h2>안녕 리액트{name}</h2>
-      </header>
+        <b id = 'bold_text'>React.js</b>
+      <MyFooter />
     </div>
   );
 }
@@ -17,3 +20,12 @@ function App() {
 //react가 주로 사용하는 es module system
 //module를 내보내면 import 이름 from 경로로 사용함
 export default App;
+
+
+/*
+JSX 규칙
+1. 닫힘규칙- <div></div> 안닫아 주면 에러, <div/> 열자마자 닫는것은 self colsing
+2. 최상위태그 규칙- return 가장 위에 코드들을 모두 감싸고 있는 태그
+최상위 태그를 만드록 싶지 않다면 import React from 'react' 한 후
+<React.Fragment></React.Fragment> 로 묶어주면됨
+*/
