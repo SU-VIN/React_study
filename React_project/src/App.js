255ce96495e343bacbffce0f76a1491c92b456f3
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-11-05 16:57:39 +0900

    데이터 수정하기

--- React_project/src/App.js
+++ React_project/src/App.js
@@ -49,16 +49,23 @@ function App() {
     setData([newItem,...data])
   }
 
-  const onDelete=(targetId)=>{
+  const onRemove=(targetId)=>{
     console.log(`${targetId}가 삭제되었습니다`);
     const newDiaryList = data.filter((it)=>it.id !== targetId);
     setData(newDiaryList);
   }
 
+  const onEdit = (targetid,newContent)=>{
+    setData(
+      data.map((it)=>it.id===targetid?{...it,content:newContent}:it)
+    )
+
+  }
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}/>
-      <DiaryList diaryList={data} onDelete={onDelete}/>
+      <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit}/>
     </div>
   );
 }
--- React_project/src/DiaryItem.js
+++ React_project/src/DiaryItem.js
@@ -1,5 +1,48 @@
+import { useRef,useState } from "react"
+
 const DiaryItem = ({
-    onDelete,author, content, created_date, emotion, id})=>{
+    onEdit,
+    onRemove,
+    author, 
+    content, 
+    created_date, 
+    emotion, 
+    id})=>{
+    //true면 수정중 
+    const [isEdit,setIsEdit]=useState(false);
+    const toggleIsEdit = ()=>setIsEdit(!isEdit);
+
+    const [localContent,setLocalContent]=useState(content);
+
+    const localContentInput =useRef()
+
+    const handleRemove = ()=>{
+        if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)){
+            onRemove(id)
+        }
+    }
+    //수정 상태에서 나간다
+    const handleQuitEdit = ()=>{
+        setIsEdit(false);
+        setLocalContent(content)
+
+    }
+
+    const handleEdit = ()=>{
+        //5글자 이상
+        if(localContent.length>5){
+            if(window.confirm(`${id}번 째 일기를 수정하시겠습니까?`)){
+                onEdit(id,localContent)
+                toggleIsEdit();
+            }
+
+        }else{
+            localContentInput.current.focus();
+
+        }
+
+    }
+
     return(
         <div className="DiaryItem">
             <div className="info">
@@ -9,14 +52,21 @@ const DiaryItem = ({
                   {new Date(created_date).toLocaleString()}  
                 </span>
             </div>
-            <div className="content">{content}</div>
-            <div className="delete">
-            <button onClick={()=>{
-                if(window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)){
-                    onDelete(id)
-                }
-            }}>삭제하기</button>
+            <div className="content">
+                {isEdit?<>
+                    <textarea ref={localContentInput} value={localContent} onChange={(e)=>setLocalContent(e.target.value)}></textarea>
+                </>:<>{content}</>}
             </div>
+
+            {isEdit? <>
+                <button onClick={handleQuitEdit}>수정 취소</button>
+                <button onClick={handleEdit}>수정 완료</button>
+            </>:<>
+                <button onClick={handleRemove}>삭제하기</button>
+                <button onClick={toggleIsEdit}>수정하기</button>
+            </>}    
+
+
         </div>
     )
 
