e54ac39540508b8a57644a5ece1d229a15197352
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-12-06 17:53:09 +0900

    update

--- js_basic/11.js
+++ js_basic/11.js
@@ -1,13 +1,13 @@
-// //비 구조화 할당
-// let arr = ["one", "two","three"]
-// // let one = arr[0]
-// // let two = arr[1]
-// // let three = arr[2]
+//비 구조화 할당
+let arr = ["one", "two","three"]
+// let one = arr[0]
+// let two = arr[1]
+// let three = arr[2]
 
 
-// //비 구조화 할당
-// let [one,two,three] = arr;
-// console.log(one,two,three)
+//비 구조화 할당
+let [one,two,three] = arr;
+console.log(one,two,three)
 
 //배열의 비구조 할당 - swap
 let a = 10;
@@ -22,5 +22,5 @@ let object = {one:"one",two:"two",three:"three",name:"임수빈"};
 // let two = object.two;
 // let three = object.three;
 
-let {name:myName,one,two,three}=object;
-console.log(one,two,three,myName)
+// let {name:myName,one,two,three}=object;
+// console.log(one,two,three,myName)
