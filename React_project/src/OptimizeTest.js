309b87156d49ad524eeefca5414a7408fc4cf59d
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2022-12-10 14:58:18 +0900

    React.memo

--- React_project/src/OptimizeTest.js
+++ React_project/src/OptimizeTest.js
@@ -1,40 +1,62 @@
 import React, { useState, useEffect } from "react";
 
-const TextView= React.memo(({text})=>{
+// const TextView= React.memo(({text})=>{
 
+//     useEffect(()=>{
+//         console.log(`update :: text : ${text}`);
+//     });
+
+//     return <div>{text}</div>;
+// });
+
+// const CountView= React.memo(({count})=>{
+
+//     useEffect(()=>{
+//         console.log(`update :: count : ${count}`);
+//     });
+
+//     return<div>{count}</div>;
+// });
+
+
+const CounterA = React.memo(({count})=>{
     useEffect(()=>{
-        console.log(`update :: text : ${text}`);
+        console.log(`counter a update:: ${count}`)
     });
-
-    return <div>{text}</div>;
+    return <div>{count}</div>;
 });
 
-const CountView= React.memo(({count})=>{
-
+//객체는 얕은비교.. 즉 값을 비교하는게아닌 주소비교를함
+const CounterB = React.memo(({obj})=>{
     useEffect(()=>{
-        console.log(`update :: count : ${count}`);
+        console.log(`counter b update:: ${obj.count}`)
     });
-
-    return<div>{count}</div>;
+    return <div>{obj.count}</div>;
 });
 
 const OptimizeTest= ()=>{
 
     const[count,setCount] = useState(1);
-    const[text,setText] = useState("");
+    const[obj,setObj] = useState({
+        count:1,
+    });
 
-    return <div style={{padding:50}}>
+    return(
+    <div style={{padding:50}}>
         <div>
-            <h2>count</h2>
-            <CountView count={count}/>
-            <button onClick={()=>setCount(count+1)}>+</button>
-        </div> 
+            <h2>Counter A</h2>
+            <CounterA count={count}/>
+            <button onClick={()=>setCount(count)}>A button</button>
+        </div>
         <div>
-            <h2>text</h2>
-            <TextView text={text}/>
-            <input value={text} onChange={(e)=>setText(e.target.value)}/>
-        </div>       
+            <h2>Counter B</h2>
+            <CounterB obj={obj}/>
+            <button onClick={()=>setObj({
+                count: obj.count
+            })}>B button</button>
+        </div>    
     </div>
+    );
 }
 
 export default OptimizeTest
