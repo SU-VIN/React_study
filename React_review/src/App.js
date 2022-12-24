f76d03d65d21fc32fa3dbc6cc36d964b70fe2331
SU_VIN <pyotnqls25@naver.com>
2022-12-24 12:00:00 +0900

    입력처리 복기

--- React_review/src/App.js
+++ React_review/src/App.js
@@ -1,18 +1,10 @@
+import DiaryEditor from './DiaryEditor';
 import './App.css';
-import Counter from './Counter';
 function App() {
-
-  const CounterProps={
-    a:1,
-    b:2,
-    c:3,
-    d:4,
-    prop:5
-  }
 
   return (
     <div className="App">
-      <Counter {...CounterProps}/>
+    <DiaryEditor/>
     </div>
   );
 }
