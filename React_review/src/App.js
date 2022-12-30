437d2a7f2d695b147677004f96157c0fdea819b9
SU_VIN <pyotnqls25@naver.com>
2022-12-30 19:17:48 +0900

    props 복기

--- React_review/src/App.js
+++ React_review/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import Counter from './Counter';
 function App() {
+
+  const CounterProps={
+    a:1,
+    b:2,
+    c:3,
+    d:4,
+    prop:5
+  }
+
   return (
     <div className="App">
-      <Counter/>
+      <Counter {...CounterProps}/>
     </div>
   );
 }
--- React_review/src/Counter.js
+++ React_review/src/Counter.js
@@ -1,13 +1,15 @@
 import React,{useState} from "react";
-const Counter = ()=>{
+import OddEvenResult from "./OddEvenResult";
+const Counter = ( {prop} )=>{
+
     //setCount 는 함수임!
-    const[count, setCount] = useState(0);
+    const[count, setCount] = useState(prop);
     
     //증가함수
     const onIncrease = ()=>{
         setCount(count+1);
     }
-    //감소함수 
+    //감소함수
     const onDecrease= ()=>{
         setCount(count-1);
     }
@@ -16,7 +18,12 @@ const Counter = ()=>{
         <h2>{count}</h2>
         <button onClick={onIncrease}>+</button>
         <button onClick={onDecrease}>-</button>
+        <OddEvenResult count={count}/>
         </div>
-    )
+    );
+
+};
+Counter.defalutProps={
+    prop:0,
 }
 export default Counter;
