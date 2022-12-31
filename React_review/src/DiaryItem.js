59081cb081c4305a742cc76a750841caa7bc13aa
SU_VIN <pyotnqls25@naver.com>
2022-12-31 12:00:00 +0900

    데이터 수정 복기

--- React_review/src/DiaryItem.js
+++ React_review/src/DiaryItem.js
@@ -1,9 +1,47 @@
-const DiaryItem=({author,
-                  content,
-                  emotion,
-                  created_date,
-                  id,
-                  onDelete})=>{
+import { useRef, useState } from "react";
+
+const DiaryItem=({
+    author,
+    content,
+    emotion,
+    created_date,
+    id,
+    onDelete,
+    onEdit,
+})=>{
+
+    const localContentInput = useRef(); 
+
+    const [isEdit,setIsEdit]=useState(false);
+    const toggleIsEdit=()=>setIsEdit(!isEdit);
+
+
+    const [localContent,setLocalContent] = useState(content);
+
+    const handelDelete =()=>{
+        if(window.confirm(`${id}번째 일기를 삭제하시겠습니까?`)){
+            onDelete(id);
+        }
+    }
+    //수정상태에서 나간다
+    const handleQuitEdit = ()=>{
+        setIsEdit(false);
+        setLocalContent(content);
+    }
+    
+    const handleEdit = ()=>{
+        if(localContent.length<5){
+            localContentInput.current.focus();
+            return;
+        }
+
+        if(window.confirm(`${id}번째 일기를 수정하시겠습니까?`)){
+            onEdit(id,localContent);
+            toggleIsEdit();
+        }
+        
+    }
+
     return( 
         <div className="DiaryItem">
             <div className="info">
@@ -11,16 +49,22 @@ const DiaryItem=({author,
                 <br/>
                 <span className="date">{new Date(created_date).toLocaleDateString()}</span>
             </div>
-            <div className="content">{content}</div>
-            <div>
-                <button onClick={()=>{
-                    if(window.confirm(`${id}번째 일기를 삭제하시겠습니까?`)){
-                        onDelete(id);
-                    }
-                }}>삭제하기</button>
+            <div className="content">
+                {isEdit?<>
+                    <textarea ref={localContentInput}value={localContent} onChange={(e)=>setLocalContent(e.target.value)}/>
+                </>:<>{content}</>}
             </div>
+            <div>
+                {isEdit?<><button onClick={handleQuitEdit}>수정 취소</button>
+                        <button onClick={handleEdit}>수정 완료</button></>:
+                        <><button onClick={handelDelete}>삭제하기</button>
+                        <button onClick={toggleIsEdit}>수정하기</button></>}
+
+            </div>
+
+
         </div>
     )
 }
 
-export default DiaryItem
+export default DiaryItem;
