ee64e112f344a1977e2313ed387265c69c3ae794
SU_VIN <pyotnqls25@naver.com>
2023-01-02 23:36:24 +0900

    useRef 복기

--- React_review/src/DiaryEditor.js
+++ React_review/src/DiaryEditor.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import React, { useRef,useState } from "react";
 
 const DiaryEditor=()=>{
     
+    const authorInput = useRef();
+    const contentInput = useRef();
+
     const [state,setState]= useState({
         author:"",
         content:"",
@@ -15,19 +18,34 @@ const DiaryEditor=()=>{
         })
     };
     
-    const handleSubmit=()=>{
-        console.log(state);
-        alert("저장 성공");
+    const handleSubmit = ()=>{
+        if(state.author.length<1){
+            authorInput.current.focus();
+            return;
+        }
+        if(state.content.length<5){
+            contentInput.current.focus();
+            return;
+        }
+        alert("저장 성공!");
     }
 
     return(
         <div className="DiaryEditor">
         <h2>오늘의 일기</h2>
         <div>
-            <input name="author" value={state.author} onChange={handleChangeState}/>
+            <input 
+                ref={authorInput}
+                name="author" 
+                value={state.author} 
+                onChange={handleChangeState}/>
         </div>
         <div>
-            <textarea name="content" value={state.content} onChange={handleChangeState}/>
+            <textarea
+                ref={contentInput} 
+                name="content" 
+                value={state.content} 
+                onChange={handleChangeState}/>
         </div>
         <div>
         <span>오늘의 감정점수 : </span>
