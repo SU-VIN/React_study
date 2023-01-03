2d12f164d53e04257d13ed563a64e9d8a68f2d0f
SU_VIN <pyotnqls25@naver.com>
2023-01-03 18:59:02 +0900

    리스트 렌더링& 데이터 추가 복기

--- React_review/src/App.js
+++ React_review/src/App.js
@@ -1,10 +1,30 @@
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
 import './App.css';
+import { useRef, useState } from 'react';
 function App() {
+  
+  const [data,setData]=useState([]);
+
+  const dataId = useRef(0);
+
+  const onCreate = (author,content,emotion)=>{
+    const created_date = new Date().getTime();
+    const newItem={
+      author,
+      content,
+      emotion,
+      created_date,
+      id: dataId.current,
+    }
+    dataId.current+=1;
+    setData([newItem,...data])
+  }
 
   return (
     <div className="App">
-    <DiaryEditor/>
+    <DiaryEditor onCreate={onCreate}/>
+    <DiaryList diaryList={data}/>
     </div>
   );
 }
