489141b7dc7f7bdeca62b609f50c7d2938942229
SU_VIN <pyotnqls25@naver.com>
2023-01-05 21:54:22 +0900

    useCallback
    useMemo와 다르게 함수를 전달함

--- React_review/src/DiaryEditor.js
+++ React_review/src/DiaryEditor.js
@@ -1,7 +1,11 @@
-import React, { useRef,useState } from "react";
+import React, { useEffect,useRef,useState } from "react";
 
 const DiaryEditor=({onCreate})=>{
     
+    useEffect(()=>{
+        console.log("render")
+    })
+
     const authorInput = useRef();
     const contentInput = useRef();
 
@@ -70,53 +74,4 @@ const DiaryEditor=({onCreate})=>{
         </div>
     )
 }
-export default DiaryEditor;
-
-// const DiaryEditor =()=> {
-
-//     const [state,setState]=useState({
-//         author:"",
-//         content:"",
-//         emotion:1,
-//     });
-//     // const [author,setAuthor]=useState("");
-//     // const [content,setContent]=useState("");
-
-//     const handleChangeState=(e)=>{
-//         setState({
-//             ...state,
-//             [e.target.name]:e.target.value,
-//         })
-//     }
-
-//     const handleSubmit=()=>{
-//         console.log(state);
-//         alert("저장 성공");
-//         }
-
-//     return(
-//     <div className="DiaryEditor">
-//     <h2>오늘의 일기</h2>
-//     <div>
-//         <input name="author"vaule={state.author} onChange={handleChangeState}/>
-//     </div>
-//     <div>
-//         <textarea name="content" value={state.content} onChange={handleChangeState}/>
-//     </div>
-//     <div>
-//     <span>오늘의 감정점수 : </span>
-//         <select name="emotion" value={state.emotion} onChange={handleChangeState}>
-//             <option value={1}>1</option>
-//             <option value={2}>2</option>
-//             <option value={3}>3</option>
-//             <option value={4}>4</option>
-//             <option value={5}>5</option>
-//         </select>
-//     </div>
-//     <div>
-//         <button onClick={handleSubmit}>일기 저장하기</button>
-//     </div>
-//     </div>
-//     )
-// };
-// export default DiaryEditor;
+export default React.memo(DiaryEditor);
